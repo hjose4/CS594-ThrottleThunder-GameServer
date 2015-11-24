f4954391c2f76fe57a3d4b2ceb20cde1c02e2567
CharlesMrl <charles.mariller@yahoo.fr>
2015-11-24 12:45:36 -0800

    Update database

--- DatabaseStructure.sql
+++ DatabaseStructure.sql
@@ -2,7 +2,7 @@
 --
 -- Host: superlunchvote.com    Database: cs454
 -- ------------------------------------------------------
--- Server version	5.6.27-0ubuntu0.15.04.1
+-- Server version 5.6.27-0ubuntu0.15.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -38,8 +38,24 @@ CREATE TABLE `base_vehicles` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `friend_relationships`
 --
+
+-- Table structure for table `players`
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `currency` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `user_name_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- Table structure for table `friend_relationships`
 
 DROP TABLE IF EXISTS `friend_relationships`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -50,7 +66,8 @@ CREATE TABLE `friend_relationships` (
   `friend_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `friend_relationship_friend_id` (`friend_id`)
+  KEY `friend_relationship_friend_id` (`friend_id`),
+ FOREIGN KEY (player_id) REFERENCES players(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -58,24 +75,9 @@ CREATE TABLE `friend_relationships` (
 -- Table structure for table `game_rankings`
 --
 
-DROP TABLE IF EXISTS `game_rankings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_rankings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `game_id` int(11) NOT NULL,
-  `player_id` int(11) NOT NULL,
-  `ranking` int(2) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `game_id_idx` (`game_id`),
-  KEY `player_id_idx` (`player_id`),
-  CONSTRAINT `dd_game_ranking_game_id` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
 
 --
--- Table structure for table `games`
 --
 
 DROP TABLE IF EXISTS `games`;
@@ -93,10 +95,62 @@ CREATE TABLE `games` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+-- Table structure for table `games`
+DROP TABLE IF EXISTS `game_rankings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game_rankings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) NOT NULL,
+  `player_id` int(11) NOT NULL,
+  `ranking` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `game_id_idx` (`game_id`),
+  KEY `player_id_idx` (`player_id`),
+  
+  CONSTRAINT `dd_game_ranking_game_id` FOREIGN KEY (`game_id`) REFERENCES `games` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `dd_game_ranking_player_id` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 --
--- Table structure for table `player_vehicles`
 --
 
+-- Table structure for table `customization`
+DROP TABLE IF EXISTS `customization`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customization` (
+  `id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `vehicle_type` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (vehicle_type) REFERENCES base_vehicles(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `player_customizations`
+--
+
+DROP TABLE IF EXISTS `player_customizations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player_customizations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_id` int(11) NOT NULL,
+  `customization_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  FOREIGN KEY (customization_id) REFERENCES customization(id),
+  FOREIGN KEY (player_id) REFERENCES players(id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+-- Table structure for table `player_vehicles`
 DROP TABLE IF EXISTS `player_vehicles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -105,35 +159,23 @@ CREATE TABLE `player_vehicles` (
   `base_id` int(11) NOT NULL,
   `player_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `paint` int(11) DEFAULT NULL,
+  `tires` int(11) DEFAULT NULL,
+  FOREIGN KEY (paint) REFERENCES player_customizations(id),
+  FOREIGN KEY (tires) REFERENCES player_customizations(id),
   PRIMARY KEY (`id`),
   KEY `player_vehicle_id` (`base_id`),
   KEY `player_vehicle_player` (`player_id`),
-  CONSTRAINT `player_vehicle_id` FOREIGN KEY (`base_id`) REFERENCES `base_vehicles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  FOREIGN KEY (`base_id`) REFERENCES `base_vehicles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`player_id`) REFERENCES `players` (`id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `players`
 --
-
-DROP TABLE IF EXISTS `players`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `players` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `currency` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `user_name_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `upgrades`
---
-
 DROP TABLE IF EXISTS `upgrades`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -176,5 +218,6 @@ CREATE TABLE `vehicle_upgrade_relationships` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
 
 -- Dump completed on 2015-11-21 23:48:23

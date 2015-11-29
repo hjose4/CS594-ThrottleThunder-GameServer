9ce12be1176e9730314a36aa19025cc8fd5535c1
ed12al <ed12al@hotmail.com>
2015-11-28 16:00:58 -0800

    added RequestFriendList

---
+++ networking/request/RequestFriendList.java
@@ -0,0 +1,23 @@
+package networking.request;
+
+// Java Imports
+import java.io.IOException;
+
+import dataAccessLayer.model.FriendshipModel;
+import networking.response.ResponseFriendList;
+
+public class RequestFriendList extends GameRequest {
+    private ResponseFriendList responseFriendList;
+
+    public RequestFriendList() {
+    	responses.add(responseFriendList = new ResponseFriendList());      
+    }
+
+    @Override
+    public void parse() throws IOException {}
+
+    @Override
+    public void doBusiness() throws Exception { 
+       responseFriendList.setFriends(FriendshipModel.getFriends(client.getPlayer()));
+    }
+}

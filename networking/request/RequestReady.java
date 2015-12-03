a9be56cca683a8264238a653593f91c0c5e961de
David Thomas <david.thomas214@yahoo.com>
2015-12-03 15:25:22 -0800

    logic is improved

--- networking/request/RequestReady.java
+++ networking/request/RequestReady.java
@@ -22,23 +22,20 @@ public class RequestReady extends GameRequest {
 		responseReady.setUsername(this.client.getPlayer().getUsername());
 		if(client.getSession() != null) {
 			if(!this.client.getPlayer().isReady()){
+				System.out.println("Player " + client.getPlayer().getUsername() + " is ready");
 				this.client.getPlayer().setReady();
-				//We need more than one player before we can start
-				if(allReady() && client.getSession().getGameClients().size() >= client.getSession().getMinNumOfPlayers()){
-					System.out.println("Okay looks like we can move on");
-					this.client.getSession().nextPhase();
-					for(Player player : client.getSession().getPlayers()) {
-						player.setNotReady();
-					}
-				}
-				
-				this.client.getSession().addResponseForAll(this.client.getPlayer().getId(), responseReady);
-			} else {
+			}
+			
+			//We need everyone ready and min num of players connected
+			if(allReady() && client.getSession().getGameClients().size() >= client.getSession().getMinNumOfPlayers()){
+				System.out.println("Okay looks like we can move on");
+				this.client.getSession().nextPhase();
 				for(Player player : client.getSession().getPlayers()) {
 					player.setNotReady();
 				}
-				this.client.getPlayer().setReady();
 			}
+			
+			this.client.getSession().addResponseForAll(this.client.getPlayer().getId(), responseReady);
 		} else {
 			responseReady.setUsername("");
 			System.out.println("Client is not in game session: "+this.getClass().getName());
